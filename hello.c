a31a5431015d20f27aa6f0ea77b567059cbfcdc9
tsallinger1993 <42591399+tsallinger1993@users.noreply.github.com>
2018-08-23 16:54:57 -0500

    Added my repository

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Kenneth, Anthony, Max
+ * Date: 2018/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Kenneth Thomason, Electrical Engineering! Max Einfalt, Computer Engineering! Anthony Sallinger, Computer Science!\n");
+
+  return 0;
+}
